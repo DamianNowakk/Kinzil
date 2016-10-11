e3de5c8acdda22aec4d64c41dd377bd38681042e
Baniek <bankai1212@gmail.com>
2016-10-12 00:27:36 +0200

    Initial commit

---
+++ app/src/main/java/engineeringwork/pl/kinzil/BluetoothFragment.java
@@ -0,0 +1,18 @@
+package engineeringwork.pl.kinzil;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BluetoothFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.bluetooth_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/engineeringwork/pl/kinzil/CounterFragment.java
@@ -0,0 +1,18 @@
+package engineeringwork.pl.kinzil;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CounterFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.counter_fragment, container, false);
+        return view;
+    }
+}
