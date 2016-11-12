ff200e11bc4320c937223e82ec4e2fa8b38625a5
Jan Kaczorowski <192@mopr.lan>
2016-11-12 21:37:42 +0100

    Added simple history view.

---
+++ app/src/main/java/engineeringwork/pl/kinzil/containers/TripArrayAdapter.java
@@ -0,0 +1,40 @@
+package engineeringwork.pl.kinzil.containers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import engineeringwork.pl.kinzil.R;
+
+public class TripArrayAdapter extends ArrayAdapter<Trip> {
+    private Context context;
+    private List<Trip> trips;
+
+    public TripArrayAdapter(Context context, int resource, ArrayList<Trip> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.trips = objects;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Trip trip = trips.get(position);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.trip_item_layout, null);
+
+        TextView date = (TextView) view.findViewById(R.id.date);
+        TextView distance = (TextView) view.findViewById(R.id.distance);
+
+        date.setText(trip.getDate());
+        distance.setText(String.valueOf(trip.getDistance()));
+
+        return view;
+    }
+}
--- app/src/main/java/engineeringwork/pl/kinzil/history/HistoryFragment.java
+++ app/src/main/java/engineeringwork/pl/kinzil/history/HistoryFragment.java
@@ -2,19 +2,46 @@ package engineeringwork.pl.kinzil.history;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import engineeringwork.pl.kinzil.R;
+import engineeringwork.pl.kinzil.containers.Trip;
+import engineeringwork.pl.kinzil.containers.TripArrayAdapter;
 
-public class HistoryFragment extends Fragment {
+public class HistoryFragment extends ListFragment implements AdapterView.OnItemClickListener {
     View view;
+    private ArrayList<Trip> historyData = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.history_fragment, container, false);
+        historyData.add(new Trip(1, "aaa", 2.0f, 2.0f, 2.0f,123,"111", "69", "chybanie00"));
+        historyData.add(new Trip(2, "aaa1", 2.0f, 2.0f, 2.0f,123,"111", "69", "chybanie00"));
+        historyData.add(new Trip(3, "aaa2", 2.0f, 2.0f, 2.0f,123,"111", "69", "chybanie00"));
+        historyData.add(new Trip(4, "aaa3", 2.0f, 2.0f, 2.0f,123,"111", "69", "chybanie00"));
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<Trip> adapter = new TripArrayAdapter(getActivity(), 0, historyData);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(),"Item: " + position, Toast.LENGTH_SHORT).show();
     }
 }
