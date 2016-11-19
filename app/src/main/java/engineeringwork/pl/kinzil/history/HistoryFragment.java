8963dbc80cffc8fc7f57a1986297914fd133d737
Damian <damian.nowakk92@gmail.com>
2016-11-19 20:34:37 +0100

    +add map to history

--- app/src/main/java/engineeringwork/pl/kinzil/history/HistoryFragment.java
+++ app/src/main/java/engineeringwork/pl/kinzil/history/HistoryFragment.java
@@ -3,6 +3,8 @@ package engineeringwork.pl.kinzil.history;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
@@ -27,9 +29,17 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -53,6 +63,12 @@ public class HistoryFragment extends ListFragment implements
     private View detailsView;
     private DatabaseHelper databaseHelper;
     private GoogleMap mMap;
+    private ArrayList<Location> locationArrayList;
+
+    public Boolean getIsDetailsViewVisible()
+    {
+        return isDetailsViewVisible;
+    }
 
     @Nullable
     @Override
@@ -78,7 +94,7 @@ public class HistoryFragment extends ListFragment implements
         getListView().setOnItemLongClickListener(this);
     }
 
-    private void populateDetailsView(Trip trip) {
+    private void populateDetailsView(final Trip trip) {
 
         ImageView time = (ImageView) detailsView.findViewById(R.id.image);
 
@@ -95,9 +111,13 @@ public class HistoryFragment extends ListFragment implements
         maxSpeed.setText(String.valueOf(trip.getMaxSpeed()));
         calories.setText(String.valueOf(trip.getCalories()));
         duration.setText(String.valueOf(trip.getTime()));
+        try {
+            setMapSecondary(trip.getMap());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         time.setImageResource(R.mipmap.ic_time_icon);
 
-        //TODO: nalozyc trase
         FragmentManager manager = getFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         SupportMapFragment fragment = new SupportMapFragment();
@@ -107,16 +127,16 @@ public class HistoryFragment extends ListFragment implements
 
         final Button button = (Button) detailsView.findViewById(R.id.routeOverlay);
         button.setOnClickListener(new View.OnClickListener(){
-            //TODO logika tego czegos
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Wcislem guzik", Toast.LENGTH_SHORT).show();
+                ((MainActivity)getActivity()).getmSectionsPagerAdapter().setMapSecondary(trip.getMap());
+                Toast.makeText(getContext(), "Wczytano mape", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void getDataFromDataBase(){
-        databaseHelper.tripInsert(new Trip(1,login,20.0f,21.0f,22.0f,11,"6h30m","11-11-2016","hahaniewiemjak"));
+        databaseHelper.tripInsert(new Trip(1,login,20.0f,21.0f,22.0f,11,"6h30m","11-11-2016","{\"Location\":[{\"Latitude\":54.3702977,\"Longitude\":18.6103648},{\"Latitude\":54.369599,\"Longitude\":18.6137419},{\"Latitude\":54.3704764,\"Longitude\":18.6183026},{\"Latitude\":54.3688096,\"Longitude\":18.6294947},{\"Latitude\":54.3574164,\"Longitude\":18.6466144},{\"Latitude\":54.3475724,\"Longitude\":18.6455632}],\"Size\":6}"));
         ArrayList<Trip> tripsToAdd = databaseHelper.getTrip(login);
         if(tripsToAdd != null){
             trips.addAll(tripsToAdd);
@@ -128,6 +148,7 @@ public class HistoryFragment extends ListFragment implements
         populateDetailsView(trips.get(position));
         ViewAnimations.expand(detailsView, view.getHeight());
         isDetailsViewVisible = true;
+        setHasOptionsMenu(false);
     }
 
     @Override
@@ -161,31 +182,80 @@ public class HistoryFragment extends ListFragment implements
         }else if(isDetailsViewVisible) {
             ViewAnimations.collapse(detailsView);
             isDetailsViewVisible = false;
+            setHasOptionsMenu(true);
         }
+    }
+
+    public void deleteMenu() {
+        setHasOptionsMenu(false);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        } else {
-            if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                mMap.setMyLocationEnabled(true);
-            }
-        }
+//        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mMap.setMyLocationEnabled(true);
+//        } else {
+//            if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION)
+//                    == PackageManager.PERMISSION_GRANTED) {
+//                mMap.setMyLocationEnabled(true);
+//            }
+//        }
 
-        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(false);
         mMap.getUiSettings().setIndoorLevelPickerEnabled(false);
         mMap.getUiSettings().setMapToolbarEnabled(false);
         mMap.getUiSettings().setRotateGesturesEnabled(false);
+        drawSecondaryLinePath();
+    }
+
+
+    public void setMapSecondary(String map) throws JSONException {
+        locationArrayList = new ArrayList<>();
+        JSONObject obj = new JSONObject(map);
+        int size =  Integer.parseInt(obj.getString("Size"));
+        JSONArray location = obj.getJSONArray("Location");
+        for(int i = 0; i < size; i++)
+        {
+            Location loc = new Location("dummyprovider");
+            String longitude = location.getJSONObject(i).getString("Longitude");
+            String latitude = location.getJSONObject(i).getString("Latitude");
+            loc.setLongitude( Double.parseDouble(longitude));
+            loc.setLatitude( Double.parseDouble(latitude));
+            locationArrayList.add(loc);
+        }
+    }
+
+    private void drawSecondaryLinePath()
+    {
+        if ( mMap == null )
+            return;
+        if ( locationArrayList == null )
+            return;
+        if ( locationArrayList.size() < 2 )
+            return;
+
+        Location location = locationArrayList.get(locationArrayList.size()/2);
+        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13);
+        mMap.animateCamera(cu);
+
+        PolylineOptions options = new PolylineOptions();
+        options.color( Color.parseColor( "#CC0000FF" ) );
+        options.width( 5 );
+        options.visible( true );
+        for ( Location locRecorded : locationArrayList )
+        {
+            options.add( new LatLng( locRecorded.getLatitude(), locRecorded.getLongitude()));
+        }
+        mMap.addPolyline( options );
+
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        menu.clear();
         MenuItem deleteDataBase = menu.add("Usuń wszystkie wycieczki");
         deleteDataBase.setOnMenuItemClickListener(this);
     }
